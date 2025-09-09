6750c68f02847a28982bc4972fe5362b92f88ec9
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-09-09 00:05:44 -0300

    Configurando UV

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ia-monitoramento-geladeira!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ treinamento/treinamento.py
@@ -0,0 +1,9 @@
+import pandas as pd;
+
+
+def treinamento():
+
+    
+
+if __name__ == "__main__":
+    treinamento()
